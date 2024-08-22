4d7f48f678be46dfe7d1e3cdc6c5471ce73c377d
selin pir <113732977+selinpir@users.noreply.github.com>
2024-08-22 23:13:04 +0300

    2208

---
+++ SQL_patikadev_8_9_10_11_12/readme10.sql
@@ -0,0 +1,25 @@
+1-city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country)
+isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+
+    SELECT city.city_name, country.country_name
+    FROM city
+    LEFT JOIN country
+    ON city.country_id = country.country_id;
+
+2-customer tablosu ile payment tablosunda bulunan payment_id ile customer
+ tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+
+    SELECT customer.first_name, customer.last_name, payment.payment_id
+    From customer
+    RIGHT join payment
+    on customer.customer_id = payment.customer_id;
+
+
+3-customer tablosu ile rental tablosunda bulunan rental_id ile customer 
+tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+    SELECT customer.first_name, customer.last_name, rental.rental_id
+    from customer
+    full join rental
+    on customer.customer_id = rental.customer_id;
+
